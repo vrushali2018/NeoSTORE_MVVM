8e2f893d56f9c432f3abfbecaddeb921f8092bb1
Vrushali Bhosale <130543195+vrushali2018@users.noreply.github.com>
2023-10-03 18:07:24 +0530

    Add files via upload

---
+++ NeoStoreMVVM/NeoStoreMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NeoStoreMVVM
+//
+//  Created by Neosoft on 03/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("New Project in MVVM")
+    }
+
+
+}
+
